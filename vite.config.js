04c272542de86cfe6fb3b4e2adfb8cd5d9207069
Colin Peynado Jr <132117593+TroytheOverlord@users.noreply.github.com>
2025-11-22 12:07:45 -0500

    fixed errors

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Contacts-Form-Demo/',  // IMPORTANT
+  base: '/Conatcts-Form-Demo/',  // IMPORTANT
 })
